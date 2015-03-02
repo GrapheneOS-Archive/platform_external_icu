76efa4407347f7adec06fdc9abf032db82618382
Fredrik Roubert <roubert@google.com>
2015-03-02 22:56:57 +0100

    Android patch: Add --omitCollationRules to genrb.
    This change was introduced in Android by the ICU 54.1 upgrade:
    
    https://android.googlesource.com/platform/external/icu/+/f9878a2
    
    Changing ICU4J test errors into warnings was introduced for ICU 55:
    
    https://android.googlesource.com/platform/external/icu/+/0543b35
    
    Changing ICU4C test errors into log messages is new for ICU 57.
    
    (cherry picked from commit 751035aa4987a2246786f2a357b128f899df7922)
    
    Change-Id: I945ddd1aca36256e965836cc3cab9f3762e58f33

--- icu4j/main/tests/collate/src/com/ibm/icu/dev/test/collator/CollationTest.java
+++ icu4j/main/tests/collate/src/com/ibm/icu/dev/test/collator/CollationTest.java
@@ -1324,7 +1324,9 @@ public class CollationTest extends TestFmwk {
             coll = new RuleBasedCollator(rules.toString());
         } catch (Exception e) {
             logln(rules.toString());
-            errln("RuleBasedCollator(rules) failed - " + e.getMessage());
+            // Android patch: Add --omitCollationRules to genrb.
+            logln("RuleBasedCollator(rules) failed - " + e.getMessage());
+            // Android patch end.
             coll = null;
         }
     }
