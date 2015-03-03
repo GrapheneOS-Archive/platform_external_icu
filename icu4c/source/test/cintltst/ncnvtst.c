d07dec708f5bb5639a41e0902f525fe293299017
Fredrik Roubert <roubert@google.com>
2015-03-03 20:21:02 +0100

    Android patch: Do not build full 2022 tables.
    This change was introduced in Android by the ICU4.4.1 upgrade, then
    amended by a separate patch and further by the ICU52 upgrade:
    
    https://android.googlesource.com/platform/external/icu/+/ea1f181
    https://android.googlesource.com/platform/external/icu/+/1075f69
    https://android.googlesource.com/platform/external/icu/+/59d709d
    
    Change-Id: I00bc902d210701e4c06cab2c7a8cdbad32e94c09

--- icu4c/source/test/cintltst/ncnvtst.c
+++ icu4c/source/test/cintltst/ncnvtst.c
@@ -188,7 +188,7 @@ static void TestSurrogateBehaviour(){
 
         int32_t offsets[] = {0,0,0,0,0,1,1,2,2,2,2,3,5 };
 
-        /*iso-2022-jp*/
+        // iso-2022-jp  android-change
         if(!convertFromU(sampleText, sizeof(sampleText)/sizeof(sampleText[0]),
                 expected, sizeof(expected), "iso-2022-jp", 0 , TRUE, U_ZERO_ERROR))
             log_err("u-> not match.\n");
@@ -197,6 +197,10 @@ static void TestSurrogateBehaviour(){
             log_err("u->  not match.\n");
     }
 
+   /* BEGIN android-removed */
+   /* To save space, Android does not build full ISO-2022-CN tables.
+      We skip the tests for ISO-2022-CN. */
+   /* 
     log_verbose("Testing for ISO-2022-cn\n");
     {
         static const UChar    sampleText[] =   { 0x4e00, 0x04e01, 0x0031, 0xd801, 0xdc01, 0x0032};
@@ -218,7 +222,7 @@ static void TestSurrogateBehaviour(){
                                     3,  
                                     5,  };
 
-        /*iso-2022-CN*/
+        // iso-2022-CN  android-change
         if(!convertFromU(sampleText, sizeof(sampleText)/sizeof(sampleText[0]),
                 expected, sizeof(expected), "iso-2022-cn", 0 , TRUE, U_ZERO_ERROR))
             log_err("u-> not match.\n");
@@ -226,6 +230,8 @@ static void TestSurrogateBehaviour(){
                 expected, sizeof(expected), "iso-2022-cn", offsets , TRUE, U_ZERO_ERROR))
             log_err("u-> not match.\n");
     }
+    */
+    /* END android-removed */
 
         log_verbose("Testing for ISO-2022-kr\n");
     {
@@ -248,7 +254,7 @@ static void TestSurrogateBehaviour(){
                               7,
                             };
 
-        /*iso-2022-kr*/
+        // iso-2022-kr  android-change
         if(!convertFromU(sampleText, sizeof(sampleText)/sizeof(sampleText[0]),
                 expected, sizeof(expected), "iso-2022-kr", 0 , TRUE, U_ZERO_ERROR))
             log_err("u-> iso-2022-kr [UCNV_DBCS] not match.\n");
@@ -441,7 +447,7 @@ static void TestErrorBehaviour(){
             log_err("u-> euc-jp [UCNV_MBCS] \n");
     }
 
-    /*iso-2022-jp*/
+    // iso-2022-jp
     log_verbose("Testing for iso-2022-jp\n");
     {
         static const UChar    sampleText[]    = { 0x0031, 0xd801};
@@ -481,7 +487,11 @@ static void TestErrorBehaviour(){
             log_err("u-> iso-2022-jp [UCNV_MBCS] \n");
     }
 
-    /*iso-2022-cn*/
+    /* BEGIN android-removed */
+    /* To save space, Android does not build full ISO-2022-CN tables.
+       We skip the tests for ISO-2022-CN. */
+    /*
+    // iso-2022-cn  android-change
     log_verbose("Testing for iso-2022-cn\n");
     {
         static const UChar    sampleText[]    = { 0x0031, 0xd801};
@@ -531,8 +541,10 @@ static void TestErrorBehaviour(){
                 expected4MBCS, sizeof(expected4MBCS), "iso-2022-cn", offsets4MBCS, FALSE, U_ZERO_ERROR))
             log_err("u-> iso-2022-cn [UCNV_MBCS] \n");
     }
+    */
+    /* END android-removed */
 
-    /*iso-2022-kr*/
+    // iso-2022-kr  android-change
     log_verbose("Testing for iso-2022-kr\n");
     {
         static const UChar    sampleText[]    = { 0x0031, 0xd801};
@@ -1572,7 +1584,7 @@ static void TestResetBehaviour(void){
                                     0x31,0x1A, 0x32};
         static const int32_t offsets1[] =  { 3,5,10,11,12};
 
-        /*iso-2022-jp*/
+        // iso-2022-jp  android-change
         if(!testConvertFromU(sampleText, sizeof(sampleText)/sizeof(sampleText[0]),
                 expected, sizeof(expected), "iso-2022-jp",  UCNV_FROM_U_CALLBACK_SUBSTITUTE , NULL, TRUE))
             log_err("u-> not match.\n");
@@ -1587,6 +1599,10 @@ static void TestResetBehaviour(void){
 
     }
 
+    /* BEGIN android-removed */
+    /* To save space, Android does not build full ISO-2022-CN tables.
+       We skip the tests for ISO-2022-CN. */
+    /*
     log_verbose("Testing Reset for ISO-2022-cn\n");
     {
         static const UChar    sampleText[] =   { 0x4e00, 0x04e01, 0x0031, 0xd801, 0xdc01, 0x0032};
@@ -1617,7 +1633,7 @@ static void TestResetBehaviour(void){
                                     };
         static const int32_t offsets1[] =  { 5,7,13,16,17};
 
-        /*iso-2022-CN*/
+        // iso-2022-CN  android-change
         if(!testConvertFromU(sampleText, sizeof(sampleText)/sizeof(sampleText[0]),
                 expected, sizeof(expected), "iso-2022-cn", UCNV_FROM_U_CALLBACK_SUBSTITUTE , NULL, TRUE))
             log_err("u-> not match.\n");
@@ -1630,6 +1646,8 @@ static void TestResetBehaviour(void){
                 offsets1, TRUE))
            log_err("iso-2022-cn -> did not match.\n");
     }
+    */
+    /* END android-removed */
 
         log_verbose("Testing Reset for ISO-2022-kr\n");
     {
@@ -1666,7 +1684,7 @@ static void TestResetBehaviour(void){
                               13, 14, 15  
                       
                             };
-        /*iso-2022-kr*/
+        // iso-2022-kr  android-change
         if(!testConvertFromU(sampleText, sizeof(sampleText)/sizeof(sampleText[0]),
                 expected, sizeof(expected), "iso-2022-kr",  UCNV_FROM_U_CALLBACK_SUBSTITUTE , NULL, TRUE))
             log_err("u-> iso-2022-kr [UCNV_DBCS] not match.\n");
--- icu4c/source/test/cintltst/nucnvtst.c
+++ icu4c/source/test/cintltst/nucnvtst.c
@@ -288,10 +288,10 @@ void addTestNewConvert(TestNode** root)
    addTest(root, &TestISO_2022_JP, "tsconv/nucnvtst/TestISO_2022_JP");
    addTest(root, &TestJIS, "tsconv/nucnvtst/TestJIS");
    addTest(root, &TestISO_2022_JP_1, "tsconv/nucnvtst/TestISO_2022_JP_1");
-   addTest(root, &TestISO_2022_JP_2, "tsconv/nucnvtst/TestISO_2022_JP_2");
+   // android-changed (no have ISO_2022_JP_2) -- addTest(root, &TestISO_2022_JP_2, "tsconv/nucnvtst/TestISO_2022_JP_2");
    addTest(root, &TestISO_2022_KR, "tsconv/nucnvtst/TestISO_2022_KR");
    addTest(root, &TestISO_2022_KR_1, "tsconv/nucnvtst/TestISO_2022_KR_1");
-   addTest(root, &TestISO_2022_CN, "tsconv/nucnvtst/TestISO_2022_CN");
+   // android-changed (no ISO-2022-CN) -- addTest(root, &TestISO_2022_CN, "tsconv/nucnvtst/TestISO_2022_CN");
    /*
     * ICU 4.4 (ticket #7314) removes mappings for CNS 11643 planes 3..7
    addTest(root, &TestISO_2022_CN_EXT, "tsconv/nucnvtst/TestISO_2022_CN_EXT");
@@ -311,7 +311,7 @@ void addTestNewConvert(TestNode** root)
    addTest(root, &TestJB5275, "tsconv/nucnvtst/TestJB5275");
    addTest(root, &TestJB5275_1, "tsconv/nucnvtst/TestJB5275_1");
 #if !UCONFIG_NO_COLLATION
-   addTest(root, &TestJitterbug981, "tsconv/nucnvtst/TestJitterbug981");
+   // android-removed (no collation tailoring rules)  -- addTest(root, &TestJitterbug981, "tsconv/nucnvtst/TestJitterbug981");
 #endif
 
    addTest(root, &TestJitterbug1293, "tsconv/nucnvtst/TestJitterbug1293");
@@ -327,8 +327,7 @@ void addTestNewConvert(TestNode** root)
 #if !UCONFIG_NO_LEGACY_CONVERSION
    addTest(root, &TestJitterbug2346, "tsconv/nucnvtst/TestJitterbug2346");
    addTest(root, &TestJitterbug2411, "tsconv/nucnvtst/TestJitterbug2411");
-   addTest(root, &TestJitterbug6175, "tsconv/nucnvtst/TestJitterbug6175");
-
+   // android-removed (no full ISO2022 CJK tables)  -- addTest(root, &TestJitterbug6175, "tsconv/nucnvtst/TestJitterbug6175");
    addTest(root, &TestIsFixedWidth, "tsconv/nucnvtst/TestIsFixedWidth");
 #endif
 }
@@ -1523,7 +1522,13 @@ static void TestAmbiguous()
     for(i=0; (name=ucnv_getAvailableName(i))!=NULL; ++i) {
         cnv=ucnv_open(name, &status);
         if(U_SUCCESS(status)) {
+            /* BEGIN android-changed. To save space Android does not build full ISO-2022-CN CJK tables. */
+            const char* cnvName = ucnv_getName(cnv, &status);
+            if (strlen(cnvName) < 8 ||
+                strncmp(cnvName, "ISO_2022_CN", 8) != 0) {
             TestAmbiguousConverter(cnv);
+            }
+            /* END android-changed */
             ucnv_close(cnv);
         } else {
             log_err("error: unable to open available converter \"%s\"\n", name);
