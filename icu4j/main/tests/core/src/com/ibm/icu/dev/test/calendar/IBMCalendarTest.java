e22b27a9e1007b77e13f6ba77389b04c802d00c4
Fredrik Roubert <roubert@google.com>
2015-03-03 18:14:45 +0100

    Android patch: CLDR data: Force default Gregorian calendar.
    This change was introduced in Android by this patch:
    
    https://android.googlesource.com/platform/external/icu/+/b844b3e
    
    And then amended by the ICU 52 upgrade:
    
    https://android.googlesource.com/platform/external/icu/+/59d709d
    
    And then amended for ICU 55:
    
    https://android.googlesource.com/platform/external/icu/+/fafa8ad
    
    And then amended for ICU 58:
    
    https://android.googlesource.com/platform/external/icu/+/047edd2
    
    (cherry picked from commit a679be1263f240c4a3b8608b6b9acbc2d03f24cb)
    
    Change-Id: Ic500c2d745086a1c1ad02611343fdd411ba49723

--- icu4j/main/tests/core/src/com/ibm/icu/dev/test/calendar/IBMCalendarTest.java
+++ icu4j/main/tests/core/src/com/ibm/icu/dev/test/calendar/IBMCalendarTest.java
@@ -332,9 +332,11 @@ public class IBMCalendarTest extends CalendarTestFmwk {
         // Thai locale
         Calendar cal = Calendar.getInstance(new ULocale("th_TH"));
         String type = cal.getType();
-        if (!type.equals("buddhist")) {
-            errln("FAIL: Buddhist calendar is not returned for locale " + cal.toString());
+        // Android patch: Force default Gregorian calendar.
+        if (!type.equals("gregorian")) {
+            errln("FAIL: Gregorian calendar is not returned for locale " + cal.toString());
         }
+        // Android patch end.
     }
 
     /**
@@ -1102,6 +1104,7 @@ public class IBMCalendarTest extends CalendarTestFmwk {
                 "th@rg=SA",		// ignore malformed rg tag, use buddhist
         };
 
+        // Android patch: Force default Gregorian calendar.
         String[] types = {
                 "gregorian",
                 "japanese",
@@ -1113,17 +1116,18 @@ public class IBMCalendarTest extends CalendarTestFmwk {
                 "buddhist",
                 "gregorian",
                 "gregorian",
-                "buddhist",
-                "buddhist",
-                "buddhist",
+                "gregorian",
+                "gregorian",
+                "gregorian",
                 "gregorian",    // iso8601 is a gregorian sub type
                 "gregorian",
-                "islamic-umalqura",
-                "islamic-umalqura",
+                "gregorian",
+                "gregorian",
                 "japanese",
-                "buddhist",
-                "buddhist",
+                "gregorian",
+                "gregorian",
         };
+        // Android patch end.
 
         for (int i = 0; i < locs.length; i++) {
             Calendar cal = Calendar.getInstance(new ULocale(locs[i]));
