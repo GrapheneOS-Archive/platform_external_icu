92364581686dd1b20a9211102942abfdacfdf287
Fredrik Roubert <roubert@google.com>
2015-03-04 17:23:52 +0100

    Android patch: CLDR data: likelySubtags.txt: Add lots of entries.
    This change was introduced in Android by the ICU51.1 upgrade:
    
    https://android.googlesource.com/platform/external/icu/+/8393335
    
    Change-Id: Ic0f50d05baa590d7c99a87808d5c8a450bb103d8

--- icu4c/source/test/cintltst/cloctst.c
+++ icu4c/source/test/cintltst/cloctst.c
@@ -4005,8 +4005,8 @@ const char* const full_data[][3] = {
     "ur"
   }, {
     "und_Arab_SN",
-    "ar_Arab_SN",
-    "ar_SN"
+    "wo_Arab_SN",  /* Google patch */
+    "wo_Arab"  /* Google patch */
   }, {
     "und_Armn",
     "hy_Armn_AM",
@@ -4137,8 +4137,8 @@ const char* const full_data[][3] = {
     "ru"
   }, {
     "und_Cyrl_KZ",
-    "ru_Cyrl_KZ",
-    "ru_KZ"
+    "kk_Cyrl_KZ",  /* Google patch */
+    "kk"  /* Google patch */
   }, {
     "und_DE",
     "de_Latn_DE",
@@ -4197,8 +4197,8 @@ const char* const full_data[][3] = {
     "am"
   }, {
     "und_Ethi_ER",
-    "am_Ethi_ER",
-    "am_ER"
+    "ti_Ethi_ER",  /* Google patch */
+    "ti_ER"  /* Google patch */
   }, {
     "und_FI",
     "fi_Latn_FI",
@@ -4253,8 +4253,8 @@ const char* const full_data[][3] = {
     "es_GT"
   }, {
     "und_GU",
-    "en_Latn_GU",
-    "en_GU"
+    "ch_Latn_GU",  /* Google patch */
+    "ch"  /* Google patch */
   }, {
     "und_GW",
     "pt_Latn_GW",
@@ -4461,8 +4461,8 @@ const char* const full_data[][3] = {
     "tr"
   }, {
     "und_Latn_ZA",
-    "en_Latn_ZA",
-    "en_ZA"
+    "af_Latn_ZA",  /* Google patch */
+    "af"  /* Google patch */
   }, {
     "und_MA",
     "ar_Arab_MA",
@@ -4485,8 +4485,8 @@ const char* const full_data[][3] = {
     "mg"
   }, {
     "und_MH",
-    "en_Latn_MH",
-    "en_MH"
+    "mh_Latn_MH",   /* Google patch */
+    "mh"  /* Google patch */
   }, {
     "und_MK",
     "mk_Cyrl_MK",
@@ -4577,12 +4577,12 @@ const char* const full_data[][3] = {
     "ne"
   }, {
     "und_NR",
-    "en_Latn_NR",
-    "en_NR"
+    "na_Latn_NR",  /* Google patch */
+    "na"  /* Google patch */
   }, {
     "und_NU",
-    "en_Latn_NU",
-    "en_NU"
+    "niu_Latn_NU",  /* Google patch */
+    "niu"  /* Google patch */
   }, {
     "und_OM",
     "ar_Arab_OM",
