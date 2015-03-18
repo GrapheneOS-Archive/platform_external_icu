cc51f1d15d60f63359c78fc4665a6e9e20aeefce
Fredrik Roubert <roubert@google.com>
2015-03-18 15:45:12 +0100

    Android patch: Additional character encoding: gsm-03.38-2000.ucm
    This change was introduced in Android by the ICU51.1 upgrade:
    
    https://android.googlesource.com/platform/external/icu/+/8393335
    
    Change-Id: Ic4d7d8e1c20a1ae69c3e590dc69467deefa238dc

---
+++ icu4c/source/data/mappings/ucmlocal.mk
@@ -0,0 +1,6 @@
+# Copyright 2008 Google Inc. All Rights Reserved.
+# Author: mscherer@google.com (Markus Scherer)
+
+# A list of additional .ucm files to build for Google.
+
+UCM_SOURCE_LOCAL=gsm-03.38-2000.ucm
