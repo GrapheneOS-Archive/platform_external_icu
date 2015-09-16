8573e215db4fb8e992d94237f0abda1e8ae31aa2
Rayhaan Jaufeerally <rayhaan@google.com>
2015-09-16 09:43:28 +0100

    Android patch: Ticket #11903: ICU DecimalFormatSymbols#setZeroDigit does not work as expected
    This is a temporary workaround until the issue has been fixed upstream:
    
    http://bugs.icu-project.org/trac/ticket/11903
    
    Change-Id: I553da949fb5c15375f12f22b98f5e59ed827d3f1

--- icu4j/main/classes/core/src/com/ibm/icu/text/DecimalFormatSymbols.java
+++ icu4j/main/classes/core/src/com/ibm/icu/text/DecimalFormatSymbols.java
@@ -212,10 +212,10 @@ public class DecimalFormatSymbols implements Cloneable, Serializable {
     public void setZeroDigit(char zeroDigit) {
         if ( digits != null ) {
             this.digits[0] = zeroDigit;
-            if (Character.digit(zeroDigit,10) == 0) {
+            // Android patch (ticket #11903) begin.
                 for ( int i = 1 ; i < 10 ; i++ ) {
                     this.digits[i] = (char)(zeroDigit+i);
-                }
+            // Android patch (ticket #11903) end.
             }
         } else {
             this.zeroDigit = zeroDigit;
