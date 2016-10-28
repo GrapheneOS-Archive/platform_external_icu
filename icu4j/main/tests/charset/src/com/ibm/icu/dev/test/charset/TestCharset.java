multi-os-engine/icu

Android patch: Skip charset tests that fail with customized data.
Suppression of ICU4C test failures was added by the ICU 51 upgrade:

https://android.googlesource.com/platform/external/icu/+/8393335

Suppression of ICU4J test failures was added for ICU 55:

https://android.googlesource.com/platform/external/icu/+/3db47ed

Change-Id: I2051f7d688ca0efa029dba4918b3e27b39c7dba1

icu4j/main/tests/charset/src/com/ibm/icu/dev/test/charset/TestCharset.java
@@ -630,7 +630,9 @@ public class TestCharset extends TestFmwk {
                 logln("finish: " + hex(finishArray));
             }
         } catch (CharacterCodingException ex) {
-            errln(converter + " roundtrip test failed: " + ex.getMessage());
+            // Android patch: Skip tests that fail with customized data.
+            warnln(converter + " roundtrip test failed: " + ex.getMessage());
+            // Android patch end.
             ex.printStackTrace(System.err);
         }
         
@@ -662,7 +664,9 @@ public class TestCharset extends TestFmwk {
                 }
             } else {
                 if (result.isError()) {
-                    errln("Error should not have occurred while encoding HZ.(" + i + ")");
+                    // Android patch: Skip tests that fail with customized data.
+                    warnln("Error should not have occurred while encoding HZ.(" + i + ")");
+                    // Android patch end.
                 }
             }
         }
@@ -811,7 +815,9 @@ public class TestCharset extends TestFmwk {
                 return;
             } catch (RuntimeException ex) {
                 if (!currentlybad) {currentlybad = true; badcount++; logln(""); }
-                errln(converter + " " + ex.getClass().getName() + ": " + ex.getMessage());
+                // Android patch: Skip tests that fail with customized data.
+                warnln(converter + " " + ex.getClass().getName() + ": " + ex.getMessage());
+                // Android patch end.
                 continue outer;
             }
             
@@ -2379,8 +2385,10 @@ public class TestCharset extends TestFmwk {
             if(!result.isError()){
                 byte[] expected = {(byte)0xA9, (byte)0xA5, (byte)0xAF, (byte)0xFE, (byte)0xA2, (byte)0xAE};
                 if(!equals(expected, out.array())){
-                    errln("Did not get the expected result for substitution bytes. Got: "+
+                    // Android patch: Skip tests that fail with customized data.
+                    warnln("Did not get the expected result for substitution bytes. Got: "+
                            hex(out.array()));
+                    // Android patch end.
                 }
                 logln("Output: "+  hex(out.array()));
             }else{

GrapheneOS-Archive/platform_external_icu

Change-Id: I3fa89c06db28a2ec633dad0d94c44c6ead42d572

icu4j/main/tests/charset/src/com/ibm/icu/dev/test/charset/TestCharset.java
@@ -642,7 +642,9 @@ public class TestCharset extends TestFmwk {
                 logln("finish: " + hex(finishArray));
             }
         } catch (CharacterCodingException ex) {
-            errln(converter + " roundtrip test failed: " + ex.getMessage());
+            // Android patch: Skip tests that fail with customized data.
+            logln(converter + " roundtrip test failed: " + ex.getMessage());
+            // Android patch end.
             ex.printStackTrace(System.err);
         }
         
@@ -674,7 +676,9 @@ public class TestCharset extends TestFmwk {
                 }
             } else {
                 if (result.isError()) {
-                    errln("Error should not have occurred while encoding HZ.(" + i + ")");
+                    // Android patch: Skip tests that fail with customized data.
+                    logln("Error should not have occurred while encoding HZ.(" + i + ")");
+                    // Android patch end.
                 }
             }
         }
@@ -825,7 +829,9 @@ public class TestCharset extends TestFmwk {
                 return;
             } catch (RuntimeException ex) {
                 if (!currentlybad) {currentlybad = true; badcount++; logln(""); }
-                errln(converter + " " + ex.getClass().getName() + ": " + ex.getMessage());
+                // Android patch: Skip tests that fail with customized data.
+                logln(converter + " " + ex.getClass().getName() + ": " + ex.getMessage());
+                // Android patch end.
                 continue outer;
             }
             
@@ -2422,8 +2428,10 @@ public class TestCharset extends TestFmwk {
             if(!result.isError()){
                 byte[] expected = {(byte)0xA9, (byte)0xA5, (byte)0xAF, (byte)0xFE, (byte)0xA2, (byte)0xAE};
                 if(!equals(expected, out.array())){
-                    errln("Did not get the expected result for substitution bytes. Got: "+
+                    // Android patch: Skip tests that fail with customized data.
+                    logln("Did not get the expected result for substitution bytes. Got: "+
                            hex(out.array()));
+                    // Android patch end.
                 }
                 logln("Output: "+  hex(out.array()));
             }else{