1c4bccfedb9d1e2bbe79b8f92974703abccb09d3
Joachim Sauer <jsauer@google.com>
2017-02-10 23:02:48 +0000

    Android patch: Increase timeout for Transliterator Test.
    TransliteratorTest.TestInstantiation() times out on bullhead with the
    CTS default timeout of 300.000 milliseconds (and is flaky on other
    devices).
    
    As a quick-fix, set a longer timeout for this specific test.
    
    This change was introduced in Android for the O release:
    
    https://android.googlesource.com/platform/external/icu/+/394a854
    https://android.googlesource.com/platform/external/icu/+/137142e
    
    Change-Id: Ifd796f6328cea090cfadcad0d6ae702c4117ddb6

--- icu4j/main/tests/translit/src/com/ibm/icu/dev/test/translit/TransliteratorTest.java
+++ icu4j/main/tests/translit/src/com/ibm/icu/dev/test/translit/TransliteratorTest.java
@@ -163,7 +163,8 @@ public class TransliteratorTest extends TestFmwk {
         }
     }
 
-    @Test
+    // Android-changed: increase timeout.
+    @Test(timeout = 3000000L)
     public void TestInstantiation() {
         long ms = System.currentTimeMillis();
         String ID;
