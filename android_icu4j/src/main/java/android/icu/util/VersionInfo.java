20d1241cf512b778bcb70f80887277ea4ef45753
Fredrik Roubert <roubert@google.com>
2017-12-05 22:17:32 +0100

    Hide new public APIs introduced by ICU 60.
    This is temporary to decouple the library update from the API update.
    
    Bug: 68208236
    Test: make
    Test: make update-api
    Change-Id: Iaf91b7b9ab8d8836e633856c790ea5c2079300ba

--- android_icu4j/src/main/java/android/icu/util/VersionInfo.java
+++ android_icu4j/src/main/java/android/icu/util/VersionInfo.java
@@ -144,6 +144,7 @@ public final class VersionInfo implements Comparable<VersionInfo>
 
     /**
      * Unicode 10.0 version
+     * @hide unsupported on Android
      */
     public static final VersionInfo UNICODE_10_0;
 
