e24629b198a0304236d154fda079b0e11e715166
Neil Fuller <nfuller@google.com>
2019-06-12 13:02:34 +0100

    Add a static library to share ICU init code
    Add a static library (androidicuinit) to enable sharing of ICU4C
    initialization code. Switch AIcu.cpp over to using it. This means
    that libcore and various command line utils now initialize ICU4C
    in the same way.
    
    This means for binaries that use AIcu_initializeIcuOrDie():
    1) They previously didn't use time zone data overlay files from
    /data and/or the time zone data mainline module but they now do.
    2) ICU4C is initialized in a different way than before: rather than
    finding the files from a given path, the files are mapped into
    memory and the memory locations are explicitly given to ICU4C.
    
    Testing:
    To check for the expected memory mapping:
    $ pmap <pid> | grep '.dat'
    with zygote and mediaserver process pids.
    
    Tested sqlite3 after executing in adb shell as above (but a sql
    operation like "CREATE TABLE" has to be attempted before ICU
    is initialized).
    
    Also inspected logcat output for the expected log messages.
    
    Bug: 120493361
    Test: build / boot
    Test: atest CtsLibcoreTestCases
    Test: See above
    Change-Id: I7c102ddafaa36e1ff81f1dfbce4f779553ba71a1

--- libandroidicu/aicu/AIcu.cpp
+++ libandroidicu/aicu/AIcu.cpp
@@ -16,28 +16,18 @@
 
 #include <aicu/AIcu.h>
 
-#include "unicode/putil.h"
-#include "unicode/uclean.h"
-#include "unicode/utypes.h"
-#include "utils/Log.h"
-
 #include <stdlib.h>
 
+#ifdef __ANDROID__
+#include <androidicuinit/IcuRegistration.h>
+#endif
+
 void AIcu_initializeIcuOrDie() {
-    const char* runtimeModulePathPrefix = getenv("ANDROID_RUNTIME_ROOT");
-    LOG_ALWAYS_FATAL_IF(runtimeModulePathPrefix == NULL,
-            "ANDROID_RUNTIME_ROOT environment variable not set");
-
-    char buf[256];
-    const int num_written = snprintf(buf, sizeof(buf), "%s/etc/icu/", runtimeModulePathPrefix);
-    LOG_ALWAYS_FATAL_IF((num_written < 0 || static_cast<size_t>(num_written) >= sizeof(buf)),
-            "Unable to construct ICU path.");
-
-    u_setDataDirectory(buf);
-    UErrorCode status = U_ZERO_ERROR;
-
-    // u_setDataDirectory doesn't try doing anything with the directory we gave
-    // it, so we'll have to call u_init to make sure it was successful.
-    u_init(&status);
-    LOG_ALWAYS_FATAL_IF(!U_SUCCESS(status), "Failed to initialize ICU %s", u_errorName(status));
+#ifdef __ANDROID__
+    androidicuinit::IcuRegistration::Register();
+#else
+    // This function is only supported when there is access to
+    // androidicuinit.
+    abort();
+#endif
 }
