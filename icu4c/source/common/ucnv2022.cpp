b35ec04ce98644aa44515f92768154ac9858c359
Victor Chang <vichang@google.com>
2021-01-19 10:15:44 +0000

    Android patch: Add noop-*.ucm to prevent 2022 security attack.
    This change was introduced in Android by this patch:
    
    https://android.googlesource.com/platform/external/icu/+/e53684e
    
    And then amended by the ICU4.4.1 and ICU51.1 upgrades:
    
    https://android.googlesource.com/platform/external/icu/+/ea1f181
    https://android.googlesource.com/platform/external/icu/+/8393335
    https://android.googlesource.com/platform/external/icu/+/1075f69
    https://android.googlesource.com/platform/external/icu/+/59d709d
    
    (cherry picked from commit bcf29ec1a5965a72d04a9385d4ff2a627528c121)
    
    Test: n/a
    Change-Id: I891406b90a1dc60a79efb6ff354709bf58223c59

--- icu4c/source/common/ucnv2022.cpp
+++ icu4c/source/common/ucnv2022.cpp
@@ -520,7 +520,9 @@ _ISO2022Open(UConverter *cnv, UConverterLoadArgs *pArgs, UErrorCode *errorCode){
             }
             if(jpCharsetMasks[version]&CSM(GB2312)) {
                 myConverterData->myConverterArray[GB2312] =
-                    ucnv_loadSharedData("ibm-5478", &stackPieces, &stackArgs, errorCode);   /* gb_2312_80-1 */
+                    /* BEGIN android-changed */
+                    ucnv_loadSharedData("noop-gb2312_gl", &stackPieces, &stackArgs, errorCode); /* gb_2312_80-1 */
+                    /* END android-changed */
             }
             if(jpCharsetMasks[version]&CSM(KSC5601)) {
                 myConverterData->myConverterArray[KSC5601] =
@@ -551,7 +553,9 @@ _ISO2022Open(UConverter *cnv, UConverterLoadArgs *pArgs, UErrorCode *errorCode){
             if(version==1) {
                 cnvName="icu-internal-25546";
             } else {
-                cnvName="ibm-949";
+                /* BEGIN android-changed */
+                cnvName="ksc_5601";
+                /* END android-changed */
                 myConverterData->version=version=0;
             }
             if(pArgs->onlyTestIsLoadable) {
@@ -595,14 +599,16 @@ _ISO2022Open(UConverter *cnv, UConverterLoadArgs *pArgs, UErrorCode *errorCode){
             }
 
             /* open the required converters and cache them */
+            /* BEGIN android-changed */
             myConverterData->myConverterArray[GB2312_1] =
-                ucnv_loadSharedData("ibm-5478", &stackPieces, &stackArgs, errorCode);
+                ucnv_loadSharedData("noop-gb2312_gl", &stackPieces, &stackArgs, errorCode);
             if(version==1) {
                 myConverterData->myConverterArray[ISO_IR_165] =
-                    ucnv_loadSharedData("iso-ir-165", &stackPieces, &stackArgs, errorCode);
+                    ucnv_loadSharedData("noop-iso-ir-165", &stackPieces, &stackArgs, errorCode);
             }
             myConverterData->myConverterArray[CNS_11643] =
-                ucnv_loadSharedData("cns-11643-1992", &stackPieces, &stackArgs, errorCode);
+                ucnv_loadSharedData("noop-cns-11643", &stackPieces, &stackArgs, errorCode);
+            /* END android-changed */
 
 
             /* set the function pointers to appropriate functions */
