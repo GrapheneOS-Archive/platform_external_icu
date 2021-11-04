8613564814153a145e2876db11c9de1932b85d4d
Almaz Mingaleev <mingaleev@google.com>
2021-11-04 14:39:49 +0000

    Rearrange methods in updateicudata.py.
    After aosp/1557177 update-tzdata.py and updateicudata.py behaviour
    slightly diverged. This CL allows to reuse that logic in
    update-tzdata.py.
    
    Bug: 204052899
    Test: followed TZDB update instructions
    
    Change-Id: If6b4d3917c1a3f2dfd9eb76f057c9a74494e3472

--- tools/updateicudata.py
+++ tools/updateicudata.py
@@ -17,10 +17,8 @@
 
 from __future__ import print_function
 
-import os
 import sys
 
-import i18nutil
 import icuutil
 
 
@@ -29,26 +27,10 @@ def main():
   icu_dir = icuutil.icuDir()
   print('Found icu in %s ...' % icu_dir)
 
-  makeIcuDataFiles()
-
-  # if icu4c/source/data/misc/langInfo.txt is re-generated, the binary data files need to be
-  # re-generated. makeIcuDataFiles() are called until it coverages because the re-generation
-  # depends icu4j, and icu4j depends on the bigit nary data files.
-  while (icuutil.RequiredToMakeLangInfo()):
-    makeIcuDataFiles()
-
+  icuutil.GenerateIcuDataFiles()
 
   print('Look in %s for new data files' % icu_dir)
   sys.exit(0)
-
-def makeIcuDataFiles():
-  i18nutil.SwitchToNewTemporaryDirectory()
-  icu_build_dir = '%s/icu' % os.getcwd()
-
-  icuutil.PrepareIcuBuild(icu_build_dir)
-
-  icuutil.MakeAndCopyIcuDataFiles(icu_build_dir)
-
 
 if __name__ == '__main__':
   main()
