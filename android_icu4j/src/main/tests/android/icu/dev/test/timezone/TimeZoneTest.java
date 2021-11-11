066871b88fe7f6253979d3fd4b8086703035d199
Almaz Mingaleev <mingaleev@google.com>
2021-11-11 12:40:01 +0000

    DO NOT MERGE Track tzdb 2021a3 update. [Q CTS]
    ICU's tzdata 2021a3 changes canonicalisation rules for some time zones.
    This CL updates tests assosiated with these changes with one minor
    change: Android skipped change which makes America/Panama canonical
    representation of America/Coral_Harbour.
    
    Only test changes are kept, binary and txt files left as they were.
    
    Bug: 204757016
    Test: see system/timezone CL
    
    Change-Id: I9442dfc4e08cd9dc55b5afe5ed5e4d9336dadfa5
    Merged-In: I9442dfc4e08cd9dc55b5afe5ed5e4d9336dadfa5

--- android_icu4j/src/main/tests/android/icu/dev/test/timezone/TimeZoneTest.java
+++ android_icu4j/src/main/tests/android/icu/dev/test/timezone/TimeZoneTest.java
@@ -552,10 +552,10 @@ public class TimeZoneTest extends TestFmwk
 
 
         String tzver = TimeZone.getTZDataVersion();
-        if (tzver.length() != 5 /* 4 digits + 1 letter */) {
-            errln("FAIL: getTZDataVersion returned " + tzver);
-        } else {
+        if (tzver != null && (tzver.length() == 5 || tzver.length() == 6) /* 4 digits + 1 or 2 letters */ ) {
             logln("PASS: tzdata version: " + tzver);
+        } else {
+            errln("FAIL: getTZDataVersion returned " + tzver);
         }
     }
 
@@ -1544,6 +1544,12 @@ public class TimeZoneTest extends TestFmwk
                 {"Africa/Sao_Tome", "Africa/Abidjan"},
                 {"America/Antigua", "America/Port_of_Spain"},
                 {"America/Anguilla", "America/Port_of_Spain"},
+                {"America/Cayman", "America/Panama"},
+                // TODO(b/204533494): enable this check back.
+                // Android-Changed: due to issues with time zones canonicity, decision was made
+                // to keep America/Coral_Harbour as alias to America/Atikokan.
+                // See more details in system/timezone/RELEASE_NOTES.md
+                // {"America/Coral_Harbour", "America/Panama"},
                 {"America/Curacao", "America/Aruba"},
                 {"America/Dominica", "America/Port_of_Spain"},
                 {"America/Grenada", "America/Port_of_Spain"},
@@ -1552,7 +1558,6 @@ public class TimeZoneTest extends TestFmwk
                 {"America/Lower_Princes", "America/Aruba"},
                 {"America/Marigot", "America/Port_of_Spain"},
                 {"America/Montserrat", "America/Port_of_Spain"},
-                {"America/Panama", "America/Cayman"},
                 {"America/Santa_Isabel", "America/Tijuana"},
                 {"America/Shiprock", "America/Denver"},
                 {"America/St_Barthelemy", "America/Port_of_Spain"},
@@ -1562,7 +1567,7 @@ public class TimeZoneTest extends TestFmwk
                 {"America/St_Vincent", "America/Port_of_Spain"},
                 {"America/Toronto", "America/Montreal"},
                 {"America/Tortola", "America/Port_of_Spain"},
-                {"America/Virgin", "America/Port_of_Spain"},
+                {"America/Virgin", "America/Puerto_Rico"},
                 {"Antarctica/South_Pole", "Antarctica/McMurdo"},
                 {"Arctic/Longyearbyen", "Europe/Oslo"},
                 {"Asia/Kuwait", "Asia/Aden"},
@@ -1653,7 +1658,7 @@ public class TimeZoneTest extends TestFmwk
             if (!bFoundCanonical) {
                 // test exclusion because of differences between Olson tzdata and CLDR
                 boolean isExcluded = false;
-                for (int k = 0; k < excluded1.length; k++) {
+                for (int k = 0; k < excluded2.length; k++) {
                     if (ids[i].equals(excluded2[k])) {
                         isExcluded = true;
                         break;
