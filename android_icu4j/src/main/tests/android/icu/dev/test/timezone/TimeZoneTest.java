31b94fb60d10c0f26fc09d014c95368396376aee
Almaz Mingaleev <mingaleev@google.com>
2022-04-13 14:45:55 +0000

    DOT NOT MERGE Ignore TimeZoneTest#TestCanonicalID [S].
    Test is part of CTS, so it should pass both on old builds (with 2021a3
    and old tzdata) and 2022a. To achieve that subset of excluded2 list
    needs to be commented out and non-obvious exemption list needs to be
    introduced to check some common part of 2022a and 2021a3.
    
    Bug: 228944103
    Bug: 228807416
    
    Test: atest CtsIcuTestCases:TimeZoneTest on ToT and ab/7679548 build
    Change-Id: I5bb2fe5088725fa209177c7c87d01bc134866083

--- android_icu4j/src/main/tests/android/icu/dev/test/timezone/TimeZoneTest.java
+++ android_icu4j/src/main/tests/android/icu/dev/test/timezone/TimeZoneTest.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Set;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -1506,6 +1507,7 @@ public class TimeZoneTest extends TestFmwk
     }
 
     @Test
+    @Ignore
     public void TestCanonicalID() {
         // Olson (IANA) tzdata used to have very few "Link"s long time ago.
         // This test case was written when most of CLDR canonical time zones are
